eb9008a15052e30ce02ae204e279c51b0da43d8e
Elie97 <lamoureuxelie@gmail.com>
2016-02-01 14:35:44 -0500

    Création du projet de base
    Ajout des composants nécéssaires au fonctionnement du projet. Les
    composants sont ajoutés dans une bibliothèque "Composants"

--- SimulationVéhicule/SimulationVéhicule/SimulationVéhicule/Game1.cs
+++ SimulationVéhicule/SimulationVéhicule/SimulationVéhicule/Game1.cs
@@ -11,40 +11,75 @@ using Microsoft.Xna.Framework.Media;
 
 namespace SimulationVéhicule
 {
-    //test
     public class Game1 : Microsoft.Xna.Framework.Game
     {
-        //TEST DE CONTROLE GITHUB
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        const float INTERVALLE_CALCUL_FPS = 1f;
+        const float INTERVALLE_MAJ_STANDARD = 1f / 60f;
+        GraphicsDeviceManager PériphériqueGraphique { get; set; }
+        SpriteBatch GestionSprites { get; set; }
+
+        RessourcesManager<SpriteFont> GestionnaireDeFonts { get; set; }
+        RessourcesManager<Texture2D> GestionnaireDeTextures { get; set; }
+        RessourcesManager<Model> GestionnaireDeModèles { get; set; }
+        RessourcesManager<Effect> GestionnaireDeShaders { get; set; }
+        Caméra CaméraJeu { get; set; }
+
+        public InputManager GestionInput { get; private set; }
 
         public Game1()
         {
-            graphics = new GraphicsDeviceManager(this);
+            PériphériqueGraphique = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            PériphériqueGraphique.SynchronizeWithVerticalRetrace = false;
+            IsFixedTimeStep = false;
+            IsMouseVisible = true;
         }
         protected override void Initialize()
         {
+            Vector3 positionCaméra = new Vector3(0, 250, 125);
+            Vector3 cibleCaméra = new Vector3(0, 0, 0);
+
+            GestionnaireDeFonts = new RessourcesManager<SpriteFont>(this, "Fonts");
+            GestionnaireDeTextures = new RessourcesManager<Texture2D>(this, "Textures");
+            GestionnaireDeModèles = new RessourcesManager<Model>(this, "Models");
+            GestionnaireDeShaders = new RessourcesManager<Effect>(this, "Effects");
+            GestionInput = new InputManager(this);
+            CaméraJeu = new CaméraSubjective(this, positionCaméra, cibleCaméra, Vector3.Up, INTERVALLE_MAJ_STANDARD);
+
+            Components.Add(GestionInput);
+            Components.Add(CaméraJeu);
+            Components.Add(new Afficheur3D(this));
+
+            Services.AddService(typeof(RessourcesManager<SpriteFont>), GestionnaireDeFonts);
+            Services.AddService(typeof(RessourcesManager<Texture2D>), GestionnaireDeTextures);
+            Services.AddService(typeof(RessourcesManager<Model>), GestionnaireDeModèles);
+            Services.AddService(typeof(RessourcesManager<Effect>), GestionnaireDeShaders);
+            Services.AddService(typeof(InputManager), GestionInput);
+            Services.AddService(typeof(Caméra), CaméraJeu);
+            GestionSprites = new SpriteBatch(GraphicsDevice);
+            Services.AddService(typeof(SpriteBatch), GestionSprites);
             base.Initialize();
-            //Commentaire Élie
-        }
-        protected override void LoadContent()
-        {
-            spriteBatch = new SpriteBatch(GraphicsDevice);
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                this.Exit();
-
+            GérerClavier();
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Red);
+            Window.Title = CaméraJeu.Position.ToString();
             base.Draw(gameTime);
+        }
+
+        private void GérerClavier()
+        {
+            if (GestionInput.EstEnfoncée(Keys.Escape))
+            {
+                Exit();
+            }
         }
     }
 }
